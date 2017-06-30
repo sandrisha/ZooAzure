42cebede6551ab9f8613db5bf053ef384f099837
Sandrisha <sandrisha@gmail.com>
2017-06-30 21:44:28 +0100

    Añado Lista de tipoAnimal

---
+++ ZooAzureApp/ZooAzureApp/assets/js/utilidades.js
@@ -0,0 +1,74 @@
+﻿type=['', 'info', 'success', 'warning', 'danger']
+mensajes = {
+    showSwal: function (type, title, text) {
+        if (type == 'aviso') {
+            swal({
+                title: title,
+                text: text,
+                type: 'warning'
+            });
+            return;
+        }
+
+        if (type == 'error') {
+            swal({
+                title: title,
+                text: text,
+                type: 'error'
+            });
+            return;
+        }
+
+        if (type == 'exito') {
+            swal({
+                title: title,
+                text: text,
+                type: 'success'
+            });
+            return;
+        }
+    },
+    checkLogin: function (email, password, cb) {
+        debugger;
+        $.ajax({
+            url: '/api/login',
+            type: "POST",
+            dataType: 'json',
+            data: {
+                email: email,
+                password: password
+            },
+            success: function (respuesta) {
+                // TODO OK
+                if (respuesta !== null && respuesta.error !== '') {
+                    //mensajes.showSwal('error', 'Atención', respuesta.error);
+                    return cb(null, respuesta.error);
+                }
+
+                if (respuesta !== null && respuesta.error === '' && respuesta.totalElementos == 0) {
+                    //mensajes.showSwal('error', 'Atención', 'Usuario inexistente o no encontrado');
+                    return cb(null, 'Usuario inexistente o no encontrado');
+                }
+
+                if (respuesta !== null && respuesta.error === '') {
+                    // HACER LA REDIRECCION AL DASHBOARD
+                    // window.location.href = "/dashboard.html";
+                    debugger;
+                    return cb(respuesta.dataUsuario[0], null);
+                }
+            },
+            error: function (respuesta) {
+                // HAY ERROR
+                mensajes.showSwal('error', 'Error en la llamada', '' + respuesta.status);
+            }
+        });
+    },
+    cargarDatosUsuario: function (cb) {
+        var datosUsuario = null;
+        var obj = localStorage.getItem('usuarioLogueado')
+        if (obj !== null && obj !== undefined) {
+            datosUsuario = JSON.parse(obj);
+        }
+        return cb(datosUsuario, null);
+    }
+}
---
+++ ZooAzureApp/ZooAzureApp/js/tiposAnimal.js
@@ -0,0 +1,110 @@
+﻿$(document).ready(function () {
+
+    function GetTiposAnimales() {
+        var urlAPI = 'http://localhost:55910/api/tipos';
+
+
+        $.get(urlAPI, function (respuesta, estado) {
+
+            $('#resultados').html('');
+
+            if (estado === 'success') {
+                var relleno = '';
+
+                relleno += '<table>';
+                relleno += '<tr>';
+                relleno += '    <th>Id</th>';
+                relleno += '    <th>Denominación</th>';
+                relleno += '    <th colspan="2">Acciones</th>';
+                relleno += '</tr>';
+
+                $.each(respuesta.data, function (indice, elemento) {
+                    relleno += '  <tr>';
+                    relleno += '      <td>' + elemento.id + '</td>';
+                    relleno += '      <td>' + elemento.denominacion + '</td>';
+                    relleno += '      <td><button id="btnEliminar" data-id="' + elemento.id + '">X</button></td>';
+                    relleno += '      <td><button id="btnUpdateTipoAnimal" data-id="' + elemento.id + '">Editar</button></td>';
+                    relleno += '  </tr>';
+                });
+                relleno += '</table>';
+                $('#resultados').append(relleno);
+            }
+        });
+    }
+
+    $('#resultados').on('click', '#btnEliminar', function () {
+        var urlAPI = 'http://localhost:55910/api/tipos';
+        var idTipoAnimal = $(this).attr('data-id');
+
+        $.ajax({
+            url: urlAPI + '/' + idTipoAnimal,
+            type: "DELETE",
+            success: function (respuesta) {
+                GetTiposAnimales();
+            },
+            error: function (respuesta) {
+                //debugger;
+                console.log(respuesta);
+            }
+        });
+
+    });
+    
+    $('#btnAddTipoAnimal').click(function () {
+        //debugger;
+        var nuevoTipoAnimal = $('#txtTipoAnimalDenominacion').val();
+        var urlAPI = 'http://localhost:55910/api/tipos';
+        var dataNuevoTipoAnimal = {
+            id: 0,
+            denominacion: nuevoTipoAnimal
+        };
+        //debugger;
+
+        $.ajax({
+            url: urlAPI,
+            type: "POST",
+            dataType: 'json',
+            data: dataNuevoTipoAnimal,
+            success: function (respuesta) {
+                //debugger;
+                GetTiposAnimales();
+                console.log(respuesta);
+            },
+            error: function (respuesta) {
+                //debugger;
+                console.log(respuesta);
+            }
+        });
+    });
+
+    $('#resultados').on('click', '#btnUpdateTipoAnimal', function () {
+
+        var urlAPI = 'http://localhost:55910/api/tipos';
+        var idTipoAnimal = $(this).attr('data-id');
+        var nuevoTipoAnimal = $('#txtNuevoTipoAnimal').val();
+
+        var dataTipoAnimalModificar = {
+            denominacion: nuevoTipoAnimal
+        };
+
+        $.ajax({
+            url: urlAPI + '/' + idTipoAnimal,
+            type: "PUT",
+            dataType: 'json',
+            data: dataTipoAnimalModificar,
+            success: function (respuesta) {
+                GetTiposAnimales();
+                console.log(respuesta);
+            },
+            error: function (respuesta) {
+                //debugger;
+                console.log(respuesta);
+            }
+        });
+    });
+
+    
+    GetTiposAnimales();
+
+});
+
